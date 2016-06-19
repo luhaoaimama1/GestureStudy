191cb8e7786298fd94597e13ccfc5af375187816
1149324777@qq.com <1149324777@qq.com>
2016-06-19 21:59:47 +0800

    Initial commit

---
+++ app/src/main/java/com/zone/gesturestudy/MainActivity.java
@@ -0,0 +1,13 @@
+package com.zone.gesturestudy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new GestureView2(this));
+    }
+}
